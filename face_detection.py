c44e322a12679014afad1eae2a3dc479befea2e1
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-09-28 04:05:03 -0400

    using @1adrianb face-alignment library to get landmark predictions

--- face_detection.py
+++ face_detection.py
@@ -1,25 +1,27 @@
 import numpy as np
 import mss
 import pyautogui
-from deepface import DeepFace
 import pyray as pr
 from operator import itemgetter
+import face_alignment
+import torch
+from PIL import Image
 
-
-def detect_faces(image):
-    faces = DeepFace.extract_faces(
-        image,
-        detector_backend="opencv",
-        enforce_detection=False,
-    )
-    return faces
+fa = face_alignment.FaceAlignment(
+    face_alignment.LandmarksType.TWO_D,
+    flip_input=False,
+    device="mps",
+    dtype=torch.bfloat16,
+    face_detector="blazeface",
+)
 
 
 def capture_screen():
     with mss.mss() as sct:
         monitor = sct.monitors[0]
-        screenshot = np.array(sct.grab(monitor))
-        return screenshot, screenshot.shape[1], screenshot.shape[0]
+        screenshot = sct.grab(monitor)
+        img = Image.frombytes("RGB", screenshot.size, screenshot.bgra, "raw", "BGRX")
+        return img, screenshot.width, screenshot.height
 
 
 def main():
@@ -42,8 +44,7 @@ def main():
 
     while not pr.window_should_close():
         screen, capture_width, capture_height = capture_screen()
-        faces = detect_faces(screen)
-        print(f"Detected {len(faces)} faces")
+        preds = fa.get_landmarks_from_image(np.array(screen))
 
         scale_x = screen_width / capture_width
         scale_y = screen_height / capture_height
@@ -51,22 +52,13 @@ def main():
         pr.begin_drawing()
         pr.clear_background(pr.BLANK)
 
-        for face in faces:
-            if face["confidence"] < 0.9:
-                continue
-            params = face["facial_area"]
-            x, y, w, h = itemgetter("x", "y", "w", "h")(params)
-
-            # Scale coordinates
-            scaled_x = int(x * scale_x)
-            scaled_y = int(y * scale_y)
-            scaled_w = int(w * scale_x)
-            scaled_h = int(h * scale_y)
-
-            print(
-                f"Face detected at ({scaled_x}, {scaled_y}) with width {scaled_w} and height {scaled_h}"
-            )
-            pr.draw_rectangle_lines(scaled_x, scaled_y, scaled_w, scaled_h, pr.RED)
+        if preds is not None:
+            for pred in preds:
+                if pred is not None:
+                    for landmark in pred:
+                        x, y = landmark[0], landmark[1]
+                        x, y = x * scale_x, y * scale_y
+                        pr.draw_circle(int(x), int(y), 2, pr.WHITE)
 
         pr.end_drawing()
     pr.close_window()
